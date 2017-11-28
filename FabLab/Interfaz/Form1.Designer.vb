cbee03a4adb9ac79a7a9a1d5385d1d9ac2205ab5
oscar <alumno@2DAMB22>
2017-11-28 14:48:18 +0100

    te pongo

--- FabLab/Interfaz/Form1.Designer.vb
+++ FabLab/Interfaz/Form1.Designer.vb
@@ -24,22 +24,24 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.UsuarioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.MáquinaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.InformesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.VentanaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.AyudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NuevoUsuarioToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GestiónDeUsuariosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MáquinaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.NuevaMáquinaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.GestiónDeMaquinasToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InformesToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VentanaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.OrganizarVentanaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.MinimizarTodosToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.AyudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.AcercaDeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
         Me.ToolStrip2 = New System.Windows.Forms.ToolStrip()
         Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
         Me.ToolStripLabel2 = New System.Windows.Forms.ToolStripLabel()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.MenuStrip1.SuspendLayout()
         Me.ToolStrip2.SuspendLayout()
         Me.SuspendLayout()
@@ -60,12 +62,36 @@ Partial Class Form1
         Me.UsuarioToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
         Me.UsuarioToolStripMenuItem.Text = "&Usuario"
         '
+        'NuevoUsuarioToolStripMenuItem
+        '
+        Me.NuevoUsuarioToolStripMenuItem.Name = "NuevoUsuarioToolStripMenuItem"
+        Me.NuevoUsuarioToolStripMenuItem.Size = New System.Drawing.Size(177, 22)
+        Me.NuevoUsuarioToolStripMenuItem.Text = "&Nuevo usuario"
+        '
+        'GestiónDeUsuariosToolStripMenuItem
+        '
+        Me.GestiónDeUsuariosToolStripMenuItem.Name = "GestiónDeUsuariosToolStripMenuItem"
+        Me.GestiónDeUsuariosToolStripMenuItem.Size = New System.Drawing.Size(177, 22)
+        Me.GestiónDeUsuariosToolStripMenuItem.Text = "&Gestión de usuarios"
+        '
         'MáquinaToolStripMenuItem
         '
         Me.MáquinaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NuevaMáquinaToolStripMenuItem, Me.GestiónDeMaquinasToolStripMenuItem})
         Me.MáquinaToolStripMenuItem.Name = "MáquinaToolStripMenuItem"
         Me.MáquinaToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
         Me.MáquinaToolStripMenuItem.Text = "&Máquina"
+        '
+        'NuevaMáquinaToolStripMenuItem
+        '
+        Me.NuevaMáquinaToolStripMenuItem.Name = "NuevaMáquinaToolStripMenuItem"
+        Me.NuevaMáquinaToolStripMenuItem.Size = New System.Drawing.Size(185, 22)
+        Me.NuevaMáquinaToolStripMenuItem.Text = "&Nueva máquina"
+        '
+        'GestiónDeMaquinasToolStripMenuItem
+        '
+        Me.GestiónDeMaquinasToolStripMenuItem.Name = "GestiónDeMaquinasToolStripMenuItem"
+        Me.GestiónDeMaquinasToolStripMenuItem.Size = New System.Drawing.Size(185, 22)
+        Me.GestiónDeMaquinasToolStripMenuItem.Text = "&Gestión de maquinas"
         '
         'InformesToolStripMenuItem
         '
@@ -79,37 +105,6 @@ Partial Class Form1
         Me.VentanaToolStripMenuItem.Name = "VentanaToolStripMenuItem"
         Me.VentanaToolStripMenuItem.Size = New System.Drawing.Size(61, 20)
         Me.VentanaToolStripMenuItem.Text = "&Ventana"
-        '
-        'AyudaToolStripMenuItem
-        '
-        Me.AyudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AcercaDeToolStripMenuItem})
-        Me.AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
-        Me.AyudaToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
-        Me.AyudaToolStripMenuItem.Text = "&Ayuda"
-        '
-        'NuevoUsuarioToolStripMenuItem
-        '
-        Me.NuevoUsuarioToolStripMenuItem.Name = "NuevoUsuarioToolStripMenuItem"
-        Me.NuevoUsuarioToolStripMenuItem.Size = New System.Drawing.Size(177, 22)
-        Me.NuevoUsuarioToolStripMenuItem.Text = "&Nuevo usuario"
-        '
-        'GestiónDeUsuariosToolStripMenuItem
-        '
-        Me.GestiónDeUsuariosToolStripMenuItem.Name = "GestiónDeUsuariosToolStripMenuItem"
-        Me.GestiónDeUsuariosToolStripMenuItem.Size = New System.Drawing.Size(177, 22)
-        Me.GestiónDeUsuariosToolStripMenuItem.Text = "&Gestión de usuarios"
-        '
-        'NuevaMáquinaToolStripMenuItem
-        '
-        Me.NuevaMáquinaToolStripMenuItem.Name = "NuevaMáquinaToolStripMenuItem"
-        Me.NuevaMáquinaToolStripMenuItem.Size = New System.Drawing.Size(185, 22)
-        Me.NuevaMáquinaToolStripMenuItem.Text = "&Nueva máquina"
-        '
-        'GestiónDeMaquinasToolStripMenuItem
-        '
-        Me.GestiónDeMaquinasToolStripMenuItem.Name = "GestiónDeMaquinasToolStripMenuItem"
-        Me.GestiónDeMaquinasToolStripMenuItem.Size = New System.Drawing.Size(185, 22)
-        Me.GestiónDeMaquinasToolStripMenuItem.Text = "&Gestión de maquinas"
         '
         'OrganizarVentanaToolStripMenuItem
         '
@@ -128,10 +123,17 @@ Partial Class Form1
         Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
         Me.ToolStripMenuItem1.Size = New System.Drawing.Size(167, 6)
         '
+        'AyudaToolStripMenuItem
+        '
+        Me.AyudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AcercaDeToolStripMenuItem})
+        Me.AyudaToolStripMenuItem.Name = "AyudaToolStripMenuItem"
+        Me.AyudaToolStripMenuItem.Size = New System.Drawing.Size(53, 20)
+        Me.AyudaToolStripMenuItem.Text = "&Ayuda"
+        '
         'AcercaDeToolStripMenuItem
         '
         Me.AcercaDeToolStripMenuItem.Name = "AcercaDeToolStripMenuItem"
-        Me.AcercaDeToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.AcercaDeToolStripMenuItem.Size = New System.Drawing.Size(132, 22)
         Me.AcercaDeToolStripMenuItem.Text = "Acerca de.."
         '
         'ToolStrip1
@@ -163,11 +165,21 @@ Partial Class Form1
         Me.ToolStripLabel2.Size = New System.Drawing.Size(73, 22)
         Me.ToolStripLabel2.Text = "- Maquinas: "
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(174, 162)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(818, 444)
+        Me.Controls.Add(Me.Button1)
         Me.Controls.Add(Me.ToolStrip2)
         Me.Controls.Add(Me.ToolStrip1)
         Me.Controls.Add(Me.MenuStrip1)
@@ -201,4 +213,6 @@ Partial Class Form1
     Friend WithEvents ToolStrip2 As ToolStrip
     Friend WithEvents ToolStripLabel1 As ToolStripLabel
     Friend WithEvents ToolStripLabel2 As ToolStripLabel
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Button1 As Button
 End Class

0ce347ffd8fb101768aebbf389546377992bd631
oscar <alumno@2DAMB13>
2017-11-29 10:34:22 +0100

    Modificacion gestionUsuarios

--- FabLab/Interfaz/GestionUsuarios.Designer.vb
+++ FabLab/Interfaz/GestionUsuarios.Designer.vb
@@ -23,7 +23,12 @@ Partial Class GestionUsuarios
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.FiltrarUsuariosTextBox = New System.Windows.Forms.TextBox()
+        Me.NuevoUsuarioButton = New System.Windows.Forms.Button()
+        Me.ConsultarUsuarioButton = New System.Windows.Forms.Button()
+        Me.EditarUsuarioButton = New System.Windows.Forms.Button()
+        Me.EliminarUsuarioButton = New System.Windows.Forms.Button()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -32,22 +37,62 @@ Partial Class GestionUsuarios
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Location = New System.Drawing.Point(12, 59)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(800, 339)
+        Me.DataGridView1.Size = New System.Drawing.Size(800, 329)
         Me.DataGridView1.TabIndex = 0
         '
-        'TextBox1
+        'FiltrarUsuariosTextBox
         '
-        Me.TextBox1.Location = New System.Drawing.Point(12, 21)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(158, 20)
-        Me.TextBox1.TabIndex = 1
+        Me.FiltrarUsuariosTextBox.Location = New System.Drawing.Point(12, 21)
+        Me.FiltrarUsuariosTextBox.Name = "FiltrarUsuariosTextBox"
+        Me.FiltrarUsuariosTextBox.Size = New System.Drawing.Size(158, 20)
+        Me.FiltrarUsuariosTextBox.TabIndex = 1
+        '
+        'NuevoUsuarioButton
+        '
+        Me.NuevoUsuarioButton.Location = New System.Drawing.Point(707, 21)
+        Me.NuevoUsuarioButton.Name = "NuevoUsuarioButton"
+        Me.NuevoUsuarioButton.Size = New System.Drawing.Size(105, 23)
+        Me.NuevoUsuarioButton.TabIndex = 2
+        Me.NuevoUsuarioButton.Text = "Nuevo usuario"
+        Me.NuevoUsuarioButton.UseVisualStyleBackColor = True
+        '
+        'ConsultarUsuarioButton
+        '
+        Me.ConsultarUsuarioButton.Location = New System.Drawing.Point(575, 405)
+        Me.ConsultarUsuarioButton.Name = "ConsultarUsuarioButton"
+        Me.ConsultarUsuarioButton.Size = New System.Drawing.Size(75, 23)
+        Me.ConsultarUsuarioButton.TabIndex = 3
+        Me.ConsultarUsuarioButton.Text = "Consultar"
+        Me.ConsultarUsuarioButton.UseVisualStyleBackColor = True
+        '
+        'EditarUsuarioButton
+        '
+        Me.EditarUsuarioButton.Location = New System.Drawing.Point(656, 405)
+        Me.EditarUsuarioButton.Name = "EditarUsuarioButton"
+        Me.EditarUsuarioButton.Size = New System.Drawing.Size(75, 23)
+        Me.EditarUsuarioButton.TabIndex = 4
+        Me.EditarUsuarioButton.Text = "Editar"
+        Me.EditarUsuarioButton.UseVisualStyleBackColor = True
+        '
+        'EliminarUsuarioButton
+        '
+        Me.EliminarUsuarioButton.Location = New System.Drawing.Point(737, 405)
+        Me.EliminarUsuarioButton.Name = "EliminarUsuarioButton"
+        Me.EliminarUsuarioButton.Size = New System.Drawing.Size(75, 23)
+        Me.EliminarUsuarioButton.TabIndex = 5
+        Me.EliminarUsuarioButton.Text = "Eliminar"
+        Me.EliminarUsuarioButton.UseVisualStyleBackColor = True
         '
         'GestionUsuarios
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(824, 451)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.EliminarUsuarioButton)
+        Me.Controls.Add(Me.EditarUsuarioButton)
+        Me.Controls.Add(Me.ConsultarUsuarioButton)
+        Me.Controls.Add(Me.NuevoUsuarioButton)
+        Me.Controls.Add(Me.FiltrarUsuariosTextBox)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "GestionUsuarios"
         Me.Text = "GestionUsuarios"
@@ -58,5 +103,10 @@ Partial Class GestionUsuarios
     End Sub
 
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents FiltrarUsuariosTextBox As TextBox
+    Friend WithEvents NuevoUsuarioButton As Button
+    Friend WithEvents ConsultarUsuarioButton As Button
+    Friend WithEvents EditarUsuarioButton As Button
+    Friend WithEvents EliminarUsuarioButton As Button
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
 End Class
